54d5dd186f49a648ad4216878390d1608bc0e53a
SihamYoubi <youbisiham5@gmail.com>
2025-11-08 18:16:51 +0100

    Implement gateway and discovery microsrvices

---
+++ gateway-service/src/main/java/net/siham/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,35 @@
+package net.siham.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	//@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("r1", predicate->predicate.path("/customers/**")
+						.uri("lb://CUSTOMER-SERVICE"))
+				.route("r2", predicate->predicate.path("/products/**")
+						.uri("lb://INVENTORY-SERVICE"))
+				.build();
+	}
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+															   DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc , dlp);
+	}
+
+}
