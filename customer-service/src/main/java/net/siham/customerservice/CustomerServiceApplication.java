9224b9f21dd2f187db4123951483cc4c9233c930
SihamYoubi <youbisiham5@gmail.com>
2025-11-26 11:16:48 +0100

    Centralize configuration in config-service

--- customer-service/src/main/java/net/siham/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/siham/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package net.siham.customerservice;
 
+import net.siham.customerservice.config.CustomerConfigParams;
 import net.siham.customerservice.entities.Customer;
 import net.siham.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
